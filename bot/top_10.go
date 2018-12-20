659c1aac8d4eda432007a33b9d65e71a0599cf80
Nick Pollett <nick.pollett@vendhq.com>
2018-12-21 12:55:41 +1300

    func, method and type stubs

--- bot/top_10.go
+++ bot/top_10.go
@@ -1,7 +1,24 @@
 package bot
 
 // Top10 determines the top 10 posts of all time by number of reactions for the year
-// and posts it as an album
+// and posts it as an album.
 func (bot *Bot) Top10() error {
+	return nil
+}
+
+// Post represents a post from the FB API.
+type Post struct{}
+
+func (bot *Bot) getAllPosts() ([]Post, error) {
+	return nil, nil
+}
+
+// getTop10Posts sorts the posts by reaction count and returns the top 10.
+func getTop10Posts(posts []Post) []Post {
+	return nil
+}
+
+// postAsAlbum posts the posts to the FB API as an album
+func (bot *Bot) postAsAlbum(posts []Post) error {
 	return nil
 }
